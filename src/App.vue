a003c1d1fae424acbffc5777d8791d23d9ebc49f
Firmansyah Adiputra <frm.adiputra@gmail.com>
2019-02-03 14:51:23 +0700

    Add login

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,24 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+<template lang="pug">
+	div
+		router-view
+		.breakpoint {{ breakpoint }}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
+	name: 'App',
+	computed: {
+		breakpoint() {
+			return this.$vuetify.breakpoint.name
+		}
+	}
 }
 </script>
+
+<style>
+.breakpoint {
+	position: fixed;
+	bottom: 0;
+	right: 0;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+v-app
+    v-content
+        v-container(fill-height)
+            v-layout(row align-center)
+                v-flex(align-self-center xs12 sm12 md6 offset-md3 xl4 offset-xl4)
+                    v-card(max-width="480" style="margin: 0 auto;")
+                        v-card-title
+                            .headline Login
+                        v-card-text
+                            v-text-field(label='Username')
+                            v-text-field(label='Password' type='password')
+                        v-card-actions
+                            v-spacer
+                            v-btn(color='primary') Sign In
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
