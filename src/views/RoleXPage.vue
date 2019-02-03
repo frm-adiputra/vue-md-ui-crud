9385bf0e597448b736da20e259300ddd6ae7ec66
Firmansyah Adiputra <frm.adiputra@gmail.com>
2019-02-03 15:12:39 +0700

    Add navigation link

--- src/views/RoleXPage.vue
+++ src/views/RoleXPage.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
-    v-app
-        v-navigation-drawer(app)
-        router-view
+	v-app
+		v-navigation-drawer(app)
+			v-toolbar(flat)
+				v-list
+					v-list-tile
+						v-list-tile-content
+							v-list-tile-title.title AppTitle
+			v-list
+				v-list-tile(to="/")
+					v-list-tile-content
+						v-list-tile-title Home
+		router-view
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
